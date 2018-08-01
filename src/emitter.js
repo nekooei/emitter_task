ad8811161e41e64ad91a0ab19f6b669710724c06
Milad Nekooei <mi.nekooei@gmail.com>
2018-08-01 16:33:26 +0430

    create off function

--- src/emitter.js
+++ src/emitter.js
@@ -7,6 +7,29 @@ let events = {
 
 export function off(eventName, fn) {
 
+    if(eventName === undefined){
+        throw new Error("eventName is required!");
+    }
+    if(typeof eventName !== 'string'){
+        throw new Error("Invalid input");
+    }
+    if(!fn && typeof fn !== 'function'){
+        throw new Error("should provide a function");
+    }
+    if(!fn){
+        events = {
+            ...events,
+            [eventName]: undefined
+        };
+    }else {
+        events = {
+            ...events,
+            [eventName]: events[eventName].func.filter(f => f.toString() !== fn.toString())
+        };
+    }
+
+
+    return events[eventName].func.length;
 }
 
 export function on(eventName, fn) {
