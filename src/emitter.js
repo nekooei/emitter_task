efe96cf9dddbd2e083f21f709eae651c9c549c1f
Milad Nekooei <mi.nekooei@gmail.com>
2018-08-01 16:33:14 +0430

    Create on and trigger function

--- src/emitter.js
+++ src/emitter.js
@@ -1,13 +1,53 @@
 // Note: The following methods should be exported for Karma tests only
 
-export function off() {
+let events = {
+
+};
+
+
+export function off(eventName, fn) {
+
 }
 
-export function on() {
+export function on(eventName, fn) {
+
+    if(eventName === undefined){
+        throw new Error("eventName is required!");
+    }
+    if(typeof eventName !== 'string' || (fn !== undefined && typeof fn !== 'function')){
+        throw new Error("Invalid input");
+    }
+
+    events = {
+        ...events,
+        [eventName] : {
+            func :  events[eventName] !== undefined ? [...events[eventName].func, fn] : [fn],
+            once : false
+        }
+    };
+    return events[eventName].func.length;
 }
 
 export function once() {
+
+    return Object.keys(events).length;
 }
 
-export function trigger() {
+export function trigger(eventName, ...args) {
+    if(eventName === undefined){
+        throw new Error("eventName is required!");
+    }
+    if(typeof eventName !== 'string'){
+        throw new Error("Invalid input");
+    }
+    if(events[eventName] !== undefined){
+
+        for(let func of events[eventName].func){
+            func(...args);
+        }
+
+        return true;
+    }else {
+        return false;
+    }
 }
